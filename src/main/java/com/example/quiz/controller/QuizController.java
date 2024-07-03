12f326502b3337c407ba1fb4d82706f7d40a120d
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-07-03 20:40:03 +0900

    feat: insert 기능 추가

--- src/main/java/com/example/quiz/controller/QuizController.java
+++ src/main/java/com/example/quiz/controller/QuizController.java
@@ -6,9 +6,13 @@ import com.example.quiz.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /** Quiz 컨트롤러 */
 @Controller
@@ -41,5 +45,25 @@ public class QuizController {
         model.addAttribute("title", "등록 폼");
 
         return "crud";
+    }
+
+    /** Quiz 데이터를 1건 등록 */
+    @PostMapping("/insert")
+    public String insert(@Validated QuizForm quizForm, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        // Formdptj Entity로 넣기
+        Quiz quiz = new Quiz();
+        quiz.setQuestion(quizForm.getQuestion());
+        quiz.setAnswer(quizForm.getAnswer());
+        quiz.setAuthor(quizForm.getAuthor());
+
+        // 입력 체크
+        if (!bindingResult.hasErrors()) {
+            service.insertQuiz(quiz);
+            redirectAttributes.addFlashAttribute("complete", "등록이 완료되었습니다.");
+            return "redirect:/quiz";
+        } else {
+            // 에러가 발생한 경우에는 목록 표시로 변경
+            return showList(quizForm, model);
+        }
     }
 }
